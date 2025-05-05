e48146af929350386c3490b3902d097c09a6b227
regi <witantoregi@gmail.com>
2025-05-05 10:47:07 +0700

    add security step to Jenkinsfile

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const x = 5;
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/about', (req, res) => {
